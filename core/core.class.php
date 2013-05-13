3466bdce64c2688c6ef5cd6afcf8247ffb35dbd2
axiuno <axiuno@gmail.com>
2013-05-13 14:47:02 +0800

    modified:   core/core.class.php

--- core/core.class.php
+++ core/core.class.php
@@ -658,6 +658,7 @@ class core {
 			header("Cache-Control: private, post-check=0, pre-check=0, max-age=0");
 			header("Pragma: no-cache");
 			header('Content-Type: text/html; charset=UTF-8');
+			header('X-Powered-By: XiunoPHP;');
 		}
 		
 	}
